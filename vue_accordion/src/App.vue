a1287dfab04813de0582d8cf4412ddeaa65cbb38
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2023-08-21 01:04:54 +0200

    Finished Accordion Animation
    Next TODO: Cleanup Project

--- vue_accordion/src/App.vue
+++ vue_accordion/src/App.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-
+import AccordionBtn from './components/AccordionBtn.vue';
+import DarkModeToggleBtn from './components/DarkModeToggleBtn.vue';
 </script>
 
 <template>
-  <div>
+  <div class=" w-screen min-h-screen bg-white dark:bg-slate-700">
+    <div class="w-full flex justify-around items-center mb-10">
+
+      <h1 class="text-black dark:text-white text-xl font-bold ml-5 mt-5 mr-1">Accordion Menu Button Demo</h1>
+      <DarkModeToggleBtn class="mr-5 mt-5 ml-1" />
+
+
+    </div>
+
+
+
+    <div class="m-5">
+      <AccordionBtn />
+    </div>
 
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@media (prefers-reduced-motion: reduce) {
+
+  * {
+    transition: none !important;
+  }
+}
+</style>
---
+++ vue_accordion/src/components/AccordionBtn.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="w-full m-0 p-0 pointer-events-none overflow-hidden">
+        <div class="main-container pointer-events-auto bg-gray-300 dark:bg-gray-500 p-3 rounded-xl flex justify-start items-center hover:cursor-pointer overflow-hidden"
+            @click="isOpen = !isOpen">
+            <i class="bx bx-money icon text-3xl"
+                :class="isOpen ? 'text-black dark:text-white' : 'text-gray-400 dark:text-gray-300'"></i>
+            <div class="title text-lg ml-3 truncate"
+                :class="isOpen ? 'text-black dark:text-white' : 'text-gray-400 dark:text-gray-300'">Payments</div>
+            <i class="bx bx-chevron-right icon chevron text-3xl ml-auto"
+                :class="isOpen ? 'open text-black dark:text-white' : 'text-gray-400 dark:text-gray-300'"></i>
+        </div>
+        <ul class="h-auto w-auto z-0">
+            <AccordionItem title="Income" :actionEvent="() => { console.log('Income') }" :isOpen="isOpen" />
+            <AccordionItem title="Expenses" :actionEvent="() => { console.log('Expenses') }" :isOpen="isOpen" />
+            <AccordionItem title="Savings" :actionEvent="() => { console.log('Savings') }" :isOpen="isOpen"
+                :isLastElement="true" />
+        </ul>
+    </div>
+</template>
+<script lang="ts">
+import AccordionItem from './AccordionItem.vue';
+export default {
+    name: 'AccordionBtn',
+    components: {
+        AccordionItem
+    },
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+
+}
+</script>
+<style scoped>
+.icon,
+.title {
+    transition: all 0.25s ease-in-out;
+}
+
+.chevron.open {
+    transform: rotate(-90deg);
+}
+
+.main-container:hover .icon,
+.main-container:hover .title {
+    color: black;
+}
+
+.dark .main-container:hover .icon,
+.dark .main-container:hover .title {
+    color: white;
+}
+</style>
